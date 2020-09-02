a2132821253d1598ef2cf58206547db84d789ab4
navic1992 <civan.erbay@web.de>
2020-09-02 16:57:59 +0200

    Add PutzPlancalculation method

--- src/app/cleaningplan/cleaningplan.component.ts
+++ src/app/cleaningplan/cleaningplan.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-cleaningplan',
   templateUrl: './cleaningplan.component.html',
-  styleUrls: ['./cleaningplan.component.scss']
+  styleUrls: ['./cleaningplan.component.scss'],
 })
-
 export class CleaningplanComponent implements OnInit {
-
   //Definition of an Array of Objects
-  memberlist: {name: string, age: string}[]
+  memberlist: { name: string; age: string }[];
 
-  
-  constructor() { }
+  //currentCleanPerson
+  currCleanPersonIndex: number
+
+  now: any = new Date();
+  onejan: any = new Date(this.now.getFullYear(), 0, 1);
+  week = Math.ceil(
+    ((this.now - this.onejan) / 86400000 + this.onejan.getDay() + 1) / 7
+  );
+  weekday: any = this.now.getDay()
+
+  constructor() {}
+    
+  //returns e.g. 36 (this.week) % 5 (this.memberlist.length) = 1 (leftover of modulo for indexposition)
+   putzPlanCalc = () => {
+    this.currCleanPersonIndex = this.week % this.memberlist.length
+}
 
   ngOnInit(): void {
     this.memberlist = [
-      { name: "Anthea", age: "29" },
-      { name: "Paco", age: "27" },
-      { name: "Civan", age: "27" },
-      { name: "Paul", age: "28" },
-      { name: "Eyleen", age: "26" }
-    ]
-  }
+      { name: 'Eyleen', age: '29' },
+      { name: 'Paco', age: '27' },
+      { name: 'Anthea', age: '27' },
+      { name: 'Paul', age: '28' },
+      { name: 'Civan', age: '26' },
+    ];
 
+ 
+
+
+
+  }
 }
