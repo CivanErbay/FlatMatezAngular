3a18b98556990ea94b104aaa006444792892449a
navic1992 <civan.erbay@web.de>
2020-09-03 17:22:21 +0200

    Add ingredients Array

--- src/app/ingredients/ingredients.component.ts
+++ src/app/ingredients/ingredients.component.ts
@@ -7,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngredientsComponent implements OnInit {
 
-  constructor() { }
+ingredients = 
+[
+  { dish: "Pizza",
+    ingOne: "Dough",
+    ingTwo: "Cheese",
+    ingTree: "Tomato",
+    duration: 15,
+    favourite: true
+},{ dish: "Falafel",
+    ingOne: "Chickpeas",
+    ingTwo: "Bread",
+    ingTree: "Herbs",
+    duration: 4,
+    favourite: true
+},{ dish: "PadThai",
+    ingOne: "Noodels",
+    ingTwo: "Peanuts",
+    ingTree: "Chicken",
+    duration: 10,
+    favourite: true
+},{ dish: "Döner",
+    ingOne: "Bread",
+    ingTwo: "Meet",
+    ingTree: "Salad",
+    duration: 3,
+    favourite: false
+},{ dish: "Pasta",
+    ingOne: "Noodels",
+    ingTwo: "Oil",
+    ingTree: "Tomato",
+    duration: 15,
+    favourite: false
+},
+]
 
+  constructor() { }
   ngOnInit(): void {
   }
 
