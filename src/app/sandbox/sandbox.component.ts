7d7b03aec5f1e4b3711724e52250d5d227d156bd
navic1992 <civan.erbay@web.de>
2020-09-03 17:48:02 +0200

    Minor changes

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,67 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogExampleComponent } from '../dialog-example/dialog-example.component';
+import { IngredientsComponent } from '../ingredients/ingredients.component';
 
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
-  styleUrls: ['./sandbox.component.scss']
+  styleUrls: ['./sandbox.component.scss'],
 })
 export class SandboxComponent implements OnInit {
-
-  currentDish = null;
+  constructor(public dialog: MatDialog) {}
 
   favDishes = [
     {
-      title: "Pizza", price: 9
-    }, {
-      title: "Falafel", price: 3
-    }, {
-      title: "PadThai", price: 7
-    }, {
-      title: "Döner", price: 4
-    }, {
-      title: "Pasta", price: 8
+      title: 'Pizza',
+      price: 9,
     },
-    
-  ]
+    {
+      title: 'Falafel',
+      price: 3,
+    },
+    {
+      title: 'PadThai',
+      price: 7,
+    },
+    {
+      title: 'Döner',
+      price: 4,
+    },
+    {
+      title: 'Pasta',
+      price: 8,
+    },
+  ];
 
-  themeColor = "red"
-  constructor(public dialog: MatDialog) { }
- 
-  ngOnInit(): void {
-  }
+  //Variables
+  themeColor = 'red';
+  currentDish = null;
+
+  ngOnInit(): void {}
 
   updateColor() {
-    console.log("Whoop")
-    this.themeColor ="salmon";
+    console.log('Whoop');
+    this.themeColor = 'salmon';
   }
 
   updateColor2() {
-    this.themeColor = "blue"
+    this.themeColor = 'blue';
   }
 
   //TODO: Up & DownVote need to be stored in State
   selectAndVoteDish(dish: string) {
-
     //Only set currendDish Variable
-    console.log("Select dish Fired", dish)
+    console.log('Select dish Fired', dish);
     this.currentDish = dish;
 
     //Opens dialogExampleComponent-html and stores Obversable in dialogRef
-    //The Second Parameter is for passing in Data into the Dialog Component (through dialog-example.components.ts -- Dependency Injection 
+    //The Second Parameter is for passing in Data into the Dialog Component (through dialog-example.components.ts -- Dependency Injection
     // constructor(@Inject(MAT_DIALOG_DATA) public data:any) { })
     // In the Template you can call {{data.name}}
-    let dialogRef = this.dialog.open(DialogExampleComponent, {data: {name: this.currentDish.title}});
-
-    //calls afterClosed Method of Dialog and subscribe ob that and shows the result 
-    //afterClosed() checks the mat-dialog-close value in the .html File of the Dialog Component
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`)
+    let dialogRef = this.dialog.open(DialogExampleComponent, {
+      data: { name: this.currentDish.title },
     });
 
+    //calls afterClosed Method of Dialog and subscribe ob that and shows the result
+    //afterClosed() checks the mat-dialog-close value in the .html File of the Dialog Component
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
-
-
-
