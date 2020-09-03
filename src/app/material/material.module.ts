cae41139135b2e6cdfaf4ce7ec1b5a9fb0a82103
navic1992 <civan.erbay@web.de>
2020-09-03 12:42:37 +0200

    Rework Landing

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule} from '@angular/material/input'
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
 
 
 
 const MaterialComponents = [
   MatInputModule,
-  MatSlideToggleModule
+  MatSlideToggleModule,
+  MatCardModule
 ]
 
 @NgModule({
